78bcecba1966601a90acacaf91fc0d1b9ef7d266
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-04 23:05:59 +0530

    new file:   eploy-prod.yml 	new file:   src/plugins/volatility.py

---
+++ src/plugins/volatility.py
@@ -0,0 +1,19 @@
+import volatility3.framework as vol
+
+class ThreatScanner:
+    def __init__(self, memory_dump):
+        self.context = vol.Context()
+        self.config = vol.requirements.VirtualAddressSpaceRequirements(
+            memory_dump
+        )
+        
+    def find_malware(self):
+        """Detects API unhooking and DLL injection"""
+        plugins = [
+            "windows.malfind.Malfind",
+            "windows.pslist.PsList"
+        ]
+        return {
+            plugin: vol.run_plugin(plugin, self.config) 
+            for plugin in plugins
+        }
