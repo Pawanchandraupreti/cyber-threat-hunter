20e6ee616b8e8bd51d6e004bddb87e0c4578dfe6
Pawan <upretipawanchandra@gmail.com>
2025-12-09 23:30:35 +0530

    modified:   core/neuro/apt_transformer.py

--- core/neuro/apt_transformer.py
+++ core/neuro/apt_transformer.py
@@ -20,5 +20,3 @@ class APTDetector(torch.nn.Module):
     def detect(self, log_sequence):
         outputs = self.model(**log_sequence)
         return [self.technique_map[i] for i in torch.argmax(outputs.logits, dim=1)]
-    
-    
