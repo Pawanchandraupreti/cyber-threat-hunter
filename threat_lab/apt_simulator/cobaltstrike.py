354352e928e968ead1e75a48cb4863c5cd1fcb8c
Pawan <upretipawanchandra@gmail.com>
2026-02-07 23:29:31 +0530

    modified:   threat_lab/apt_simulator/cobaltstrike.py

--- threat_lab/apt_simulator/cobaltstrike.py
+++ threat_lab/apt_simulator/cobaltstrike.py
@@ -14,5 +14,3 @@ class BeaconSimulator:
             payload = f"session={random.getrandbits(128)}&data={os.urandom(64).hex()}"
             send(IP(dst=server)/TCP(dport=443)/Raw(load=payload))  # Fixed TCP instead of ICP
             time.sleep(self.jitter())
-
-            
